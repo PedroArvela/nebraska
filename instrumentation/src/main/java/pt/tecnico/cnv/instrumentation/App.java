4b9350b0b4a540d53151898f8861726efceda8ab
Pedro Arvela <jp.arvela@gmail.com>
2016-04-01 01:30:20 +0100

    Initial Instrumentation commit

---
+++ instrumentation/src/main/java/pt/tecnico/cnv/instrumentation/App.java
@@ -0,0 +1,14 @@
+package pt.tecnico.cnv.instrumentation;
+
+import BIT.highBIT.*;
+import BIT.lowBIT.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
