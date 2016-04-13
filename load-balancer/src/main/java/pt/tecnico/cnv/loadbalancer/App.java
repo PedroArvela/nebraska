3b67e15600f07d993153ca043a7afdf3f93cf09c
Jose Pedro de Almeida Arvela <ist173557@lab11p5.rnl.tecnico.ulisboa.pt>
2016-04-13 14:56:12 +0100

    New load balancer, yay!

--- load-balancer/src/main/java/pt/tecnico/cnv/loadbalancer/App.java
+++ load-balancer/src/main/java/pt/tecnico/cnv/loadbalancer/App.java
@@ -1,62 +1,75 @@
 package pt.tecnico.cnv.loadbalancer;
 
-import java.util.ArrayList;
-import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.cloudwatch.AmazonCloudWatchClient;
+import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2Client;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
 import com.amazonaws.services.ec2.model.Reservation;
-import com.amazonaws.services.elasticloadbalancing.AmazonElasticLoadBalancingClient;
-import com.amazonaws.services.elasticloadbalancing.model.CreateLoadBalancerRequest;
-import com.amazonaws.services.elasticloadbalancing.model.CreateLoadBalancerResult;
-import com.amazonaws.services.elasticloadbalancing.model.Listener;
-import com.amazonaws.services.elasticloadbalancing.model.RegisterInstancesWithLoadBalancerRequest;
-import com.amazonaws.services.elasticloadbalancing.model.RegisterInstancesWithLoadBalancerResult;
 
 public class App {
-	public static void main(String[] args) {
-		AmazonEC2Client ec2 = new AmazonEC2Client();
-		AmazonElasticLoadBalancingClient elb = new AmazonElasticLoadBalancingClient();
+	static AmazonEC2 ec2;
+	static AmazonCloudWatchClient cloudWatch;
 
-		// Create Load Balancer
-		CreateLoadBalancerRequest lbRequest = new CreateLoadBalancerRequest();
-		lbRequest.setLoadBalancerName("loader");
-		List<Listener> listeners = new ArrayList<Listener>(1);
-		listeners.add(new Listener("HTTP", 80, 80));
-		lbRequest.withAvailabilityZones("us-west-2");
-		lbRequest.setListeners(listeners);
+	public static void init() throws Exception {
+		AWSCredentials credentials = null;
 
-		CreateLoadBalancerResult lbResult = elb.createLoadBalancer(lbRequest);
-		System.out.println("created load balancer loader");
+		/**
+		 * Initializes the AmazonEC2Client and AmazonCloudWatchClient with the
+		 * required credentials.
+		 * 
+		 * Requires the <code>~/.aws/credentials</code> file to be properly
+		 * placed and filled in.
+		 */
+		try {
+			credentials = new ProfileCredentialsProvider().getCredentials();
+		} catch (Exception e) {
+			throw new AmazonClientException("Cannot load the credentials from the credential profiles file. "
+					+ "Please make sure that your credentials file is at the correct "
+					+ "location (~/.aws/credentials), and is in valid format.", e);
+		}
+		ec2 = new AmazonEC2Client(credentials);
+		cloudWatch = new AmazonCloudWatchClient(credentials);
+	}
 
-//		// Get running instances
-//		DescribeInstancesResult describeInstancesRequest = ec2.describeInstances();
-//		List<Reservation> reservations = describeInstancesRequest.getReservations();
-//
-//		List<Instance> instances = new ArrayList<Instance>();
-//
-//		for (Reservation reservation : reservations) {
-//			instances.addAll(reservation.getInstances());
-//		}
-//
-//		// Get instance id's
-//		String id;
-//		List instanceId = new ArrayList();
-//		List instanceIdString = new ArrayList();
-//
-//		for (Instance in : instances) {
-//			id = in.getInstanceId();
-//			instanceId.add(new com.amazonaws.services.elasticloadbalancing.model.Instance(id));
-//			instanceIdString.add(id);
-//		}
-//
-//		// Register the instances to the balancer
-//		RegisterInstancesWithLoadBalancerRequest register = new RegisterInstancesWithLoadBalancerRequest();
-//		register.setLoadBalancerName("loader");
-//		register.setInstances((Collection) instanceId);
-//		RegisterInstancesWithLoadBalancerResult registerWithLoadBalancerResult = elb
-//				.registerInstancesWithLoadBalancer(register);
+	public static void main(String[] args) throws Exception {
+		// Initialize the EC2 Client and CloudWatch Client
+		init();
+
+		try {
+			// Set the US Oregon endpoint, which is the one used so far
+			// The complete list is at
+			// https://docs.aws.amazon.com/general/latest/gr/rande.html
+			ec2.setEndpoint("ec2.us-west-2.amazonaws.com");
+			cloudWatch.setEndpoint("monitoring.us-west-2.amazonaws.com");
+
+			// Get list of reservations
+			DescribeInstancesResult describeInstancesResult = ec2.describeInstances();
+			List<Reservation> reservations = describeInstancesResult.getReservations();
+			Set<Instance> instances = new HashSet<Instance>();
+
+			// Get list of instances from reservations
+			for (Reservation reservation : reservations) {
+				instances.addAll(reservation.getInstances());
+			}
+
+			System.out.println("Total reservations:\t" + reservations.size());
+			System.out.println("Total instances:\t" + instances.size());
+
+		} catch (AmazonServiceException e) {
+			System.out.println("Caught Exception: " + e.getMessage());
+			System.out.println("Reponse Status Code: " + e.getStatusCode());
+			System.out.println("Error Code: " + e.getErrorCode());
+			System.out.println("Request ID: " + e.getRequestId());
+		}
+
 	}
 }
