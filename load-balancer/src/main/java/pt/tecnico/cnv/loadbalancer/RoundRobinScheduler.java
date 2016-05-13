d3d92d036ffffe70af8d3db80a3eed3852318d7d
Pedro Arvela <jp.arvela@gmail.com>
2016-05-13 17:23:41 +0100

    Add timer task to periodically refresh list

--- load-balancer/src/main/java/pt/tecnico/cnv/loadbalancer/RoundRobinScheduler.java
+++ load-balancer/src/main/java/pt/tecnico/cnv/loadbalancer/RoundRobinScheduler.java
@@ -2,6 +2,8 @@ package pt.tecnico.cnv.loadbalancer;
 
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -15,13 +17,22 @@ public class RoundRobinScheduler implements Scheduler {
 	Set<Instance> insts;
 	Iterator<Instance> instanceIt;
 
-	NotificationListener notifications;
+	TimerTask task = new TimerTask() {
+		@Override
+		public void run() {
+			sendNotification();
+			Timer timer = new Timer();
+			timer.schedule(this, 15000);
+		}
+	};
 
 	public RoundRobinScheduler(NebraskaEC2Client nec2) {
 		this.nec2 = nec2;
 		this.insts = nec2.getInstances();
 
-		notifications = new NotificationListener(8080, this);
+		NotificationListener notifications = new NotificationListener(8080, this);
+		new Thread(notifications).start();
+		new Thread(task).start();
 	}
 
 	@Override
@@ -41,6 +52,7 @@ public class RoundRobinScheduler implements Scheduler {
 
 	@Override
 	public void sendNotification() {
+		System.out.println("Fetching server info remotelly");
 		this.insts = nec2.getInstances();
 	}
 }
